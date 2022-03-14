cc5b8c19a47a6abf8fadec824761ca4b8da7e679
Diego Joao Costa Santiago <djcs@motorola.com>
2022-03-14 15:56:53 -0300

    Ramp-up Scheduler Update 14 March meeting

--- Rampup/Scheduler/halide/bilateral_denoise.hpp
+++ Rampup/Scheduler/halide/bilateral_denoise.hpp
@@ -54,17 +54,17 @@ namespace {
 
             output_y(x, y, c) = sum_y(x, y, c)/sum_y(x, y, channel_min-1);
 
-            diff_x(x, y, i, c) = absd(guide_bound(x, y, c), guide_bound(x + i, y, c));
-            norm_x(x, y, i) = u16_sat((diff_x(x, y, i, 0) + diff_x(x, y, i, 1) + diff_x(x, y, i, 2))*max14_f32);
-            weights_x(x, y, i) = weights_spatial(i) * weights_range(norm_x(x, y, i));
+            diff_x(x, y, i, c) = absd(guide_bound(x, y, c), guide_bound(x + i, y, c)); //inline?
+            norm_x(x, y, i) = u16_sat((diff_x(x, y, i, 0) + diff_x(x, y, i, 1) + diff_x(x, y, i, 2))*max14_f32); //inline?
+            weights_x(x, y, i) = weights_spatial(i) * weights_range(norm_x(x, y, i)); //compute?
 
             // sum_x(x, y, c) = 0.f;
-            sum_x(x, y, c) += select(c == channel_min-1, weights_x(x, y, kernel),
+            sum_x(x, y, c) += select(c == channel_min-1, weights_x(x, y, kernel), //compute
                                 weights_x(x, y, kernel) * output_y(x + kernel, y, c));
 
-            output_x(x, y, c) = sum_x(x, y, c)/sum_x(x, y, channel_min-1);
+            output_x(x, y, c) = sum_x(x, y, c)/sum_x(x, y, channel_min-1); //inline
 
-            output(x, y, c) = clamp(output_x(x, y, c), 0.f, 1.f);
+            output(x, y, c) = clamp(output_x(x, y, c), 0.f, 1.f); //compute_root
         }
 
         void schedule() {
@@ -79,38 +79,87 @@ namespace {
                 const int parallel_size = 256;
                 Var xo{"xo"}, xi{"xi"};
 
-                output.compute_root()
-                    .bound(c, channel_min, channel_extent)
-                    .split(x, xo, xi, vector_size)
-                    .reorder(xi, c, xo, y)
-                    .parallel(y)
-                ;
-                weights_spatial.compute_root()
-                    .split(i, xo, xi, parallel_size)
-                    .parallel(xo)
-                    .vectorize(xi, vector_size)
-                ;
-                weights_range.compute_root()
-                    .split(i, xo, xi, parallel_size)
-                    .parallel(xo)
-                    .vectorize(xi, vector_size)
-                ;
-                sum_x.compute_at(output, xo)
-                    .vectorize(x, vector_size)
-                ;
-                sum_x.update(0)
-                    .split(x, xo, xi, vector_size)
-                    .reorder(xi, c, kernel, xo, y)
-                    .unroll(c)
-                ;
-                sum_y.compute_at(output, y)
-                    .vectorize(x, vector_size)
-                ;
-                sum_y.update(0)
-                    .split(x, xo, xi, vector_size)
-                    .reorder(xi, c, xo, kernel, y)
-                    .unroll(c)
-                ;
+                switch (scheduler)
+                {
+                case 3:
+                    weights_x.compute_at(output, xo)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, i, xo, y)
+                        .vectorize(xi)
+                    ;
+                    weights_y.compute_at(output, y)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, xo, i, y)
+                        .vectorize(xi)
+                    ;
+                    goto scheduler_2;
+
+                case 4:
+                    weights_x.compute_at(sum_x, xo)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, i, xo, y)
+                        .vectorize(xi)
+                    ;
+                    weights_y.compute_at(sum_y, kernel)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, xo, i, y)
+                        .vectorize(xi)
+                    ;
+                    goto scheduler_2;
+
+                case 5:
+                case 1:
+                default:
+                    weights_x.compute_at(sum_x, kernel)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, i, xo, y)
+                        .vectorize(xi)
+                    ;
+                    weights_y.compute_at(sum_y, xo)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, xo, i, y)
+                        .vectorize(xi)
+                    ;
+
+                case 2:
+                scheduler_2:
+                    output.compute_root()
+                        .bound(c, channel_min, channel_extent)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, c, xo, y)
+                        .parallel(y)
+                        .vectorize(xi)
+                    ;
+                    weights_spatial.compute_root()
+                        .split(i, xo, xi, parallel_size)
+                        .parallel(xo)
+                        .vectorize(xi, vector_size)
+                    ;
+                    weights_range.compute_root()
+                        .split(i, xo, xi, parallel_size)
+                        .parallel(xo)
+                        .vectorize(xi, vector_size)
+                    ;
+                    sum_x.compute_at(output, xo)
+                        .vectorize(x, vector_size)
+                    ;
+                    sum_x.update(0)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, c, kernel, xo, y)
+                        .unroll(c)
+                        .vectorize(xi)
+                    ;
+                    sum_y.compute_at(output, y)
+                        .vectorize(x, vector_size)
+                    ;
+                    sum_y.update(0)
+                        .split(x, xo, xi, vector_size)
+                        .reorder(xi, c, xo, kernel, y)
+                        .unroll(c)
+                        .vectorize(xi)
+                    ;
+                    break;
+                }
             }
         }
     };
